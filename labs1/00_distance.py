f5a9f249e05275e9785072e550d7e360efc04437
BogdanSumarokov <bogdansumarokov62528@gmail.com>
2023-12-30 15:43:11 +0300

    feat: add solution for task

--- labs1/00_distance.py
+++ labs1/00_distance.py
@@ -16,6 +16,14 @@ distances = {}
 
 # TODO здесь заполнение словаря
 
+for city in sites.keys():
+    for other in sites.keys():
+        if city != other:
+            x1, y1 = sites[city][0], sites[city][1]
+            x2, y2 = sites[other][0], sites[other][1]
+            if city + '-' + other not in distances and other + '-' + city not in distances:
+                distances[city + '-' + other] = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
 print(distances)
 
 

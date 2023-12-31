3a0c514944facac44e736735ef2757c6c615e69d
BogdanSumarokov <bogdansumarokov62528@gmail.com>
2023-12-31 12:40:59 +0300

    feat: add solution for 01_days_in_month.py task

--- labs4/01_days_in_month.py
+++ labs4/01_days_in_month.py
@@ -13,3 +13,11 @@ month = int(user_input)
 print('Вы ввели', month)
 
 # TODO здесь ваш код
+if user_input not in ['1','2','3','4','5','6','7','8','9','10','11','12']:
+    print("Вы ввели некорректный номер месяца!")
+elif int(user_input) == 2:
+    print(28)
+elif int(user_input) in [1,3,5,7,8,10,12]:
+    print(31)
+else:
+    print(30)
